e5baa8118fa544286643fda90f3c8e5c3446cf55
damir <damirkaniev905@gmail.com>
2022-04-06 20:57:57 +0600

    Initial Commit

---
+++ contacts_realm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contacts_realm
+//
+//  Created by admin on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
